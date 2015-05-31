8ebf28f86595b48b77b29847924e04e5b7fd51aa
Luciano Quaranta <Luciano_40@hotmail.it>
2015-05-31 16:15:15 +0200

    Update Customer.java
    sistemati alcuni import

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -3,7 +3,6 @@ package model;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
